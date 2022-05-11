c96731a233ffdcfadcd30ac46b834f4053b7e731
yasmina mohammed <ym_slih2001@students.iugaza.edu.ps>
2022-05-11 04:51:58 +0300

    first release correct2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,11 +14,11 @@
 
     <!-- Fonts -->
     {{-- <link rel="dns-prefetch" href="//fonts.gstatic.com"> --}}
-    <link href="{{asset('css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{secure_asset('css/all.min.css')}}" rel="stylesheet" type="text/css">
 
 
     <!-- Custom styles for this template-->
-    <link href="{{asset('css/sb-admin.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('css/sb-admin.css')}}" rel="stylesheet">
     <style>
         input#inputEmail, input#inputPassword {
             height: 50px;
