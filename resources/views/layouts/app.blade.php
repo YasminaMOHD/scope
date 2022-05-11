744686224eaa71efbe866841191a158e9f1ec67f
yasmina mohammed <ym_slih2001@students.iugaza.edu.ps>
2022-05-11 05:48:03 +0300

    first release correct10

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>BrainClick LeadSystem - Login</title>
 
@@ -13,11 +14,11 @@
 
     <!-- Fonts -->
     {{-- <link rel="dns-prefetch" href="//fonts.gstatic.com"> --}}
-    <link href="{{secure_asset('css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/all.min.css')}}" rel="stylesheet" type="text/css">
 
 
     <!-- Custom styles for this template-->
-    <link href="{{secure_asset('css/sb-admin.css')}}" rel="stylesheet">
+    <link href="{{asset('css/sb-admin.css')}}" rel="stylesheet">
     <style>
         input#inputEmail, input#inputPassword {
             height: 50px;
