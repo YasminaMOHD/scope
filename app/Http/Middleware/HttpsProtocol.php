f5b6e7d83e82e9b4bbcf9ee9faa56d2c24d77f94
yasmina mohammed <ym_slih2001@students.iugaza.edu.ps>
2022-05-11 06:52:53 +0300

    first release correct18

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,10 +16,10 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure() && env('APP_ENV') === 'production') {
+        // if (!$request->secure() && env('APP_ENV') === 'production') {
             return redirect()->secure($request->getRequestUri());
-         }
+        //  }
 
-         return $next($request);
+        //  return $next($request);
     }
 }
