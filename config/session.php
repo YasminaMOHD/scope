a83962b2de9f79aacbc9f69e6e5e0ac05daa69d8
yasmina mohammed <ym_slih2001@students.iugaza.edu.ps>
2022-05-11 05:40:08 +0300

    first release correct8

--- config/session.php
+++ config/session.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => env('SESSION_DOMAIN', 'http://leadsystembyscoperealestate.herokuapp.com/login'),
 
     /*
     |--------------------------------------------------------------------------
