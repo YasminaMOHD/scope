ecb86d18ad2a395769e2a1e1526b27a51c87e99e
yasmina mohammed <ym_slih2001@students.iugaza.edu.ps>
2022-05-12 13:11:56 +0300

    first release correct34

--- resources/views/Admin/Pages/test.blade.php
+++ resources/views/Admin/Pages/test.blade.php
@@ -693,7 +693,7 @@
                                             <td>{!!$lead->created_at!!}</td>
                                             <td><a href="tel:{!!$lead->phone!!}">{!!$lead->phone!!}</a></td>
                                             <td>{!!$lead->project->name ?? '' !!}</td>
-                                            @php $agen = App\Models\Agents_lead::whereIn($lead->id,'leads')->first()->agent_id;  @endphp
+                                            @php $agen = App\Models\Agents_lead::whereJsonContains('leads',$lead->id)->first()->agent_id;  @endphp
                                             <td>{!!$agen !!}</td>
                                             <td>
                                                 <div class="btn @if($lead->status->name == 'No Answer') btn-primary @elseif($lead->status->name == 'Potential') btn-light
