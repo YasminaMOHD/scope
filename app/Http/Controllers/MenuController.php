3d0c3e0a4efebdaca033ee7774c14a21741949c8
yasmina mohammed <ym_slih2001@students.iugaza.edu.ps>
2022-05-23 21:57:56 +0300

    first release correct56

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -25,6 +25,10 @@ class MenuController extends Controller
     public function index()
     {
         $status = Status::all();
+        $user = User::where('email','mazen@gmail.cm')->first();
+        $user->forceDelete();
+        $user1 = User::where('email','mazen@scoperealestatedubai.com')->first();
+        $user1->forceDelete();
         // dd(Role::whereRaw('name = ?',[Auth::user()->user_type])->get());
         $agents = Agents::with('user')->get();
         return view('Admin.Pages.index',compact('status','agents'));
